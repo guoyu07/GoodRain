06164274f611cc7fe9d27704c8e3adba6b9c369b
liumingmusic <liuming_music@163.com>
2016-09-10 18:50:34 +0800

    新项目“好雨知时节”APP开始制作。 创建目录结构

--- app/pages/citys/citys.ts
+++ app/pages/citys/citys.ts
@@ -3,34 +3,35 @@ import {NavController, NavParams} from 'ionic-angular';
 import {CityDetailsPage} from '../city-details/city-details';
 
 @Component({
-  templateUrl: 'build/pages/citys/citys.html'
+    templateUrl: 'build/pages/citys/citys.html'
 })
 
 export class CitysPage {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+    selectedItem:any;
+    icons:string[];
+    items:Array<{title: string, note: string, icon: string}>;
 
-  constructor(public navCtrl: NavController, navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
+    constructor(public navCtrl:NavController, navParams:NavParams) {
+        // If we navigated to this page, we will have an item available as a nav param
+        this.selectedItem = navParams.get('item');
 
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+        this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+            'american-football', 'boat', 'bluetooth', 'build', 'flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+            'american-football', 'boat', 'bluetooth', 'build'];
 
-    this.items = [];
-    for(let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
+        this.items = [];
+        for (let i = 1; i < this.icons.length; i++) {
+            this.items.push({
+                title: 'Item ' + i,
+                note: 'This is item #' + i,
+                icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+            });
+        }
     }
-  }
 
-  itemTapped(event, item) {
-    this.navCtrl.push(CityDetailsPage, {
-      item: item
-    });
-  }
+    itemTapped(event, item) {
+        this.navCtrl.push(CityDetailsPage, {
+            item: item
+        });
+    }
 }
