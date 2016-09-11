646fa3a09374236b61fb3eb9571490587b9762ba
liumingmusic <liuming_music@163.com>
2016-09-11 16:37:08 +0800

    1.调用接口，根据经纬度获取当前地区的天气信息；

--- app/pages/city-details/city-details.ts
+++ app/pages/city-details/city-details.ts
@@ -1,14 +1,19 @@
 import {Component} from '@angular/core';
-import {NavController, NavParams} from 'ionic-angular';
+import {NavController, NavParams, ViewController} from 'ionic-angular';
 
 @Component({
-  templateUrl: 'build/pages/city-details/city-details.html'
+    templateUrl: 'build/pages/city-details/city-details.html'
 })
 export class CityDetailsPage {
-  selectedItem: any;
 
-  constructor(public navCtrl: NavController, navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-  }
+    selectedItem:any;
+
+    constructor(public navCtrl:NavController, navParams:NavParams, public viewCtrl:ViewController) {
+        // If we navigated to this page, we will have an item available as a nav param
+        this.selectedItem = navParams.get('item');
+    }
+
+    ionViewWillEnter() {
+        this.viewCtrl.setBackButtonText('');
+    }
 }
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -8,7 +8,7 @@ import {BasePath} from '../../base/basePath';
 })
 export class HomePage {
 
-    geocodes = new CurrentPosition();
+    geocodes = new CurrentWeather();
 
     constructor(private http:Http) {
     }
@@ -18,59 +18,95 @@ export class HomePage {
         var basePath = new BasePath(this.http);
         Geolocation.getCurrentPosition().then((resp) => {
             //远程调用接口获取地理编码
-            this.http.get(basePath.getGeocodesUrl(resp.coords.longitude, resp.coords.latitude))
+            this.http.get(basePath.getWeatherUrl(resp.coords.longitude, resp.coords.latitude))
                 .subscribe(data => {
-                    var result = JSON.parse(data["_body"]);
-                    if (result.infocode === "10000") {
-                        //接下来获取当前地区的天气信息
-                        var regeocode = result.regeocode;
-                        //构造函数赋值
-                        this.geocodes = new CurrentPosition(
-                            regeocode.addressComponent.country,
-                            regeocode.addressComponent.province,
-                            regeocode.addressComponent.city,
-                            regeocode.addressComponent.district,
-                            regeocode.addressComponent.township,
-                            regeocode.addressComponent.adcode,
-                            regeocode.addressComponent.citycode,
-                            regeocode.addressComponent.towncode,
-                            regeocode.formatted_address
+                    var geo = JSON.parse(data["_body"]);
+                    if (geo["resultcode"] === "200") {
+                        var result = geo["result"];
+                        var today = result["today"];
+                        //构造函数封装数据信息
+                        this.geocodes = new CurrentWeather(
+                            today.city,
+                            today.date_y,
+                            today.week,
+                            today.temperature,
+                            today.weather,
+                            today.wind,
+                            today.dressing_index,
+                            today.dressing_advice,
+                            today.travel_index,
+                            today.exercise_index,
+                            resp.coords.longitude,
+                            resp.coords.latitude
                         );
                     }
                 });
         });
     }
 
-    //进入页面进行初始化定位信息
-    ionViewDidEnter() {
+    //进入页面进行初始化定位信息,先从缓存数据中拿数据信息，没有在进行定位拿数据信息
+    ionViewWillEnter() {
         this.doLocation();
+        /**
+         this.geocodes = new CurrentWeather(
+         "龙泉驿",
+         "2016年10月9日",
+         "星期六",
+         "8℃~20℃",
+         "晴转多云",
+         "西南风微风",
+         "较冷",
+         "建议着大衣、呢外套加毛衣、卫衣等服装。",
+         "适宜",
+         "适宜",
+         "106.234561",
+         "50.2345321");
+         **/
     }
 
 }
 
+
 //定位地区实体信息
-class CurrentPosition {
-
-    country:any;
-    province:any;
+class CurrentWeather {
+    /*城市*/
     city:any;
-    district:any;
-    township:any;
-    adcode:any;
-    citycode:any;
-    towncode:any;
-    formatted_address:any;
+    /*日期*/
+    date_y:any;
+    /*星期*/
+    week:any;
+    /*今日温度*/
+    temperature:any;
+    /*今日天气*/
+    weather:any;
+    /*风力*/
+    wind:any;
+    /*穿衣指数*/
+    dressing_index:any;
+    /*穿衣建议*/
+    dressing_advice:any;
+    /*旅游指数*/
+    travel_index:any;
+    /*晨练指数*/
+    exercise_index:any;
+    /*经度*/
+    longitude:any;
+    /*纬度*/
+    latitude:any;
 
-    constructor(country?, province?, city?, district?, township?, adcode?, citycode?, towncode?, formatted_address?) {
-        this.country = country;
-        this.province = province;
+    constructor(city?, date_y?, week?, temperature?, weather?, wind?, dressing_index?, dressing_advice?, travel_index?, exercise_index?, longitude?, latitude?) {
         this.city = city;
-        this.district = district;
-        this.township = township;
-        this.adcode = adcode;
-        this.citycode = citycode;
-        this.towncode = towncode;
-        this.formatted_address = formatted_address;
+        this.date_y = date_y;
+        this.week = week;
+        this.temperature = temperature;
+        this.weather = weather;
+        this.wind = wind;
+        this.dressing_advice = dressing_advice;
+        this.dressing_index = dressing_index;
+        this.travel_index = travel_index;
+        this.exercise_index = exercise_index;
+        this.longitude = longitude;
+        this.latitude = latitude;
     }
 
 }
